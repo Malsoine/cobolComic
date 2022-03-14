37bc132f4877b93b371c6984185582ef9a0b89ec
Malsoine <66721141+Malsoine@users.noreply.github.com>
2022-03-14 14:48:52 +0100

    Add files via upload

--- BoutiqueComic.cob
+++ BoutiqueComic.cob
@@ -14,7 +14,7 @@
            select fventes assign to "ventes.dat"
            organization indexed
            access mode is dynamic
-           record key is fv_id
+           record key is fv_cle
            alternate record key is fv_datevente WITH DUPLICATES
            file status is cr_fventes.
 
@@ -29,7 +29,7 @@
            organization indexed
            access mode is dynamic
            record key is fc_id
-           alternate record key is fc_ptFidelite WITH DUPLICATES
+           alternate record key is fc_ptsFidelite WITH DUPLICATES
            file status is cr_fclients.
 
 
@@ -45,49 +45,65 @@
                 02 fa_quantite PIC 9(4).
                 02 fa_prixAchat PIC 9(6)v99.
                 02 fa_nomFournisseur PIC A(30).
-       FD felecAge.
-           01 tamp_felecAge.
-                02 fea_idE PIC 9(15).
-                02 fea_nom PIC A(30).
-                02 fea_prenom PIC A(30).
-                02 fea_age PIC 9(3).
-                02 fea_villeHabitation PIC A(30).
-       FD fbureau.
-           01 tamp_fbureau.
-               02 fbu_code PIC A(4).
-               02 fbu_ville PIC A(30).
-               02 fbu_heure_fermeture PIC 9(2).
-               02 fbu_heure_ouverture PIC 9(2).
-               02 fbu_region PIC 9(2).
+       FD fventes.
+           01 tamp_fvente.
+                02 fv_cle.
+                    03 fv_id PIC 9(15).
+                    03 fv_statut PIC 9(15).
+                02 fv_dateVente PIC X(10).
+                02 fv_titreComics PIC A(30).
+                02 fv_prixVente PIC 9(6)v99.
+                02 fv_client PIC 9(15).
+       FD finventaire.
+           01 tamp_finventaire.
+               02 fi_id PIC 9(15).
+               02 fi_titre PIC A(30).
+               02 fi_auteur PIC A(30).
+               02 fi_quantite PIC 9(4).
+               02 fi_prix PIC 9(6)v99.
+       FD fclients.
+           01 tamp_fclient.
+               02 fc_id PIC 9(15).
+               02 fc_prenom PIC A(30).
+               02 fc_nom PIC A(30).
+               02 fc_tel PIC 9(10).
+               02 fc_mail PIC A(30).
+               02 fc_ptsFidelite PIC 9(3).
 
 
        WORKING-STORAGE SECTION.
-           77 cr_felec PIC 9(2).
-           77 cr_fbureau PIC 9(2).
-           77 cr_felecAge PIC 9(2).
-           77 Wfin PIC 9.
-           77 wStop PIC 9.
-           77 test_nom PIC 9.
-           77 test_ville PIC 9.
-           01 elec.
-               02 idE PIC 9(15).
-               02 nom PIC A(30).
-               02 prenom PIC A(30).
-               02 age PIC 9(3).
-               02 villeHabitation PIC A(30).
-           01 bureau.
-               02 bcode PIC A(4).
-               02 ville PIC A(30).
-               02 heure_fermeture PIC 9(2).
-               02 heure_ouverture PIC 9(2).
-               02 region PIC 9(2).
+           77 cr_fachats PIC 9(2).
+           77 cr_fventes PIC 9(2).
+           77 cr_finventaire PIC 9(2).
+           77 cr_fclients PIC 9(2).
+
        PROCEDURE DIVISION.
-      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-       MAIN-PROCEDURE.
-      **
-      * The main procedure of the program
-      **
-            DISPLAY "Hello world"
+      **Achats
+           OPEN I-O fachats
+           IF cr_fachats=35 THEN
+               OPEN OUTPUT fachats
+           END-IF
+           CLOSE fachats
+      **Ventes
+           OPEN I-O fventes
+           IF cr_fventes = 35 THEN
+               OPEN OUTPUT fventes
+           END-IF
+           CLOSE fventes
+      **Inventaire
+           OPEN I-O finventaire
+           IF cr_finventaire=35 THEN
+               OPEN OUTPUT finventaire
+           END-IF
+           CLOSE finventaire
+      **Clients
+           OPEN I-O fclients
+           IF cr_fclients=35 THEN
+               OPEN OUTPUT fclients
+           END-IF
+           CLOSE fclients
+
+            DISPLAY "Fichiers créés "
             STOP RUN.
       ** add other procedures here
        END PROGRAM boutiqueComic.
