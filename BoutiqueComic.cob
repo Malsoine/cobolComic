c3de0cccf5aa54c09656cc703aebe38085149806
enoraLD <101568720+enoraLD@users.noreply.github.com>
2022-04-04 16:07:53 +0200

    Add files via upload

--- BoutiqueComic.cob
+++ BoutiqueComic.cob
@@ -32,11 +32,9 @@
            alternate record key is fc_ptsFidelite WITH DUPLICATES
            file status is cr_fclients.
 
+DATA DIVISION.
 
-
-       DATA DIVISION.
-
-       FILE SECTION.
+FILE SECTION.
        FD fachats.
            01 tamp_fachats.
                 02 fa_id PIC 9(15).
@@ -71,15 +69,14 @@
                02 fc_ptsFidelite PIC 9(3).
 
 
-       WORKING-STORAGE SECTION.
+WORKING-STORAGE SECTION.
            77 cr_fachats PIC 9(2).
            77 cr_fventes PIC 9(2).
            77 cr_finventaire PIC 9(2).
            77 cr_fclients PIC 9(2).
-           77 testNomClient PIC 9.
-           77 fichierFin PIC 9.
-           77 choixSupprClient PIC 9.
-           77 testClient PIC 9.
+           77 trouve PIC 9(1).
+           77 idRef PIC 9(15).
+           77 titreRef PIC A(30).
            01 achat.
                 02 ac_id PIC 9(15).
                 02 ac_dateAchat PIC X(10).
@@ -108,17 +105,24 @@
                02 cl_tel PIC 9(10).
                02 cl_mail PIC A(30).
                02 cl_ptsFidelite PIC 9(3).
-       PROCEDURE DIVISION.
+           
+
+PROCEDURE DIVISION.
+
+        MAIN-PROCEDURE.
+
            OPEN I-O fachats
            IF cr_fachats=35 THEN
                OPEN OUTPUT fachats
            END-IF
            CLOSE fachats
+
            OPEN I-O fventes
-           IF cr_fventes = 35 THEN
+           IF cr_fventes=35 THEN
                OPEN OUTPUT fventes
            END-IF
            CLOSE fventes
+
            OPEN I-O finventaire
            IF cr_finventaire=35 THEN
                OPEN OUTPUT finventaire
@@ -131,89 +135,9 @@
            END-IF
            CLOSE fclients
 
-            DISPLAY "Fichiers créés "
 
-           *>PERFORM AJOUTCLIENT
-           PERFORM SUPPRCLIENT
-       STOP RUN.
+STOP RUN.
+   
+           COPY 'inventaire.cpy'.
+                
 
-
-           AJOUTCLIENT.
-           OPEN INPUT fclients
-           MOVE 0 TO fichierFin
-           MOVE 0 TO testClient
-           MOVE 0 TO testNomClient
-           DISPLAY "Entrez le code client :"
-           ACCEPT cl_id
-           DISPLAY "Entrez le nom :"
-           ACCEPT cl_nom
-           DISPLAY "Entrez le prénom :"
-           ACCEPT cl_prenom
-           DISPLAY "Entrez le numéro de téléphone :"
-           ACCEPT cl_tel
-           DISPLAY "Entrez l'email :"
-           ACCEPT cl_mail
-           DISPLAY "Entrez le nombre de point de fidélité : "
-           ACCEPT cl_ptsFidelite
-           PERFORM WITH TEST AFTER UNTIL fichierFin=1
-               READ fclients NEXT
-               AT END MOVE 1 TO fichierFin
-               NOT AT END
-                   IF fc_nom = cl_nom AND fc_prenom = cl_prenom THEN
-                       MOVE 1 TO testNomClient
-                   END-IF
-               END-READ
-           END-PERFORM
-
-           MOVE cl_id TO fc_id
-           READ fclients
-           KEY IS fc_id
-           INVALID KEY MOVE 0 TO testClient
-           NOT INVALID KEY MOVE 1 TO testClient
-           END-READ
-
-
-           CLOSE fclients
-           IF testClient = 1 OR testNomClient = 1 THEN
-               DISPLAY "Erreur, le client est déjà dans le fichier"
-           ELSE IF testClient = 0 AND testNomClient = 0 THEN
-               OPEN I-O fclients
-               MOVE client TO tamp_fclient
-               WRITE tamp_fclient
-               END-WRITE
-               DISPLAY "Ajout effectué"
-               DISPLAY fc_prenom
-               CLOSE fclients
-           END-IF.
-
-           SUPPRCLIENT.
-           MOVE 0 to choixSupprClient
-           MOVE 0 TO fichierFin
-           MOVE 0 TO testNomClient
-           DISPLAY "Supprimer avec l'id (1) ou nom/prénom (2) ?"
-           ACCEPT choixSupprClient
-           EVALUATE choixSupprClient
-           WHEN 1
-               DISPLAY "oui"
-           WHEN 2
-                DISPLAY "Suppression par nom et prénom"
-                DISPLAY "Entrez le nom"
-                ACCEPT cl_nom
-                DISPLAY "Entrez le prénom"
-                ACCEPT cl_prenom
-                PERFORM WITH TEST AFTER UNTIL fichierFin=1
-                   READ fclients NEXT
-                   AT END MOVE 1 TO fichierFin
-                   NOT AT END
-                   IF fc_nom = cl_nom AND fc_prenom = cl_prenom THEN
-                       MOVE 1 TO testNomClient
-                   END-IF
-                   END-READ
-               END-PERFORM
-               IF testNomClient = 1
-                   DELETE fclients
-                   DISPLAY "Suppresion effectuée"
-                END-IF
-           WHEN OTHER
-                   DISPLAY "Choix invalide"
-           END-EVALUATE.
